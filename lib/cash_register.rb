be904f6a5aa622dc9df06ce3fa092b840c8984e1
Jonathan <jonathanmoorevla@gmail.com>
2022-11-12 19:31:25 -0600

    Build methods, pass tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
 
+class CashRegister
+    attr_accessor :discount, :total, :items, :last_trans
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_trans
+    end
+    def total
+        @total
+    end
+    def add_item(title, price, quantity = 1)
+        @total += (price * quantity)
+        @items.concat([title] * quantity)
+        @last_trans = price * quantity
+    end
+    def apply_discount
+        if @discount != 0 then
+            @total -= ((@total * @discount)/100)
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    def void_last_transaction
+            @total -= @last_trans 
+    end
+end
